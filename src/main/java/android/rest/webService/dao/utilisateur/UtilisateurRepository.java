42172b0557d4b4b1a88ffea3997060ba84a4e973
Sofian Khalfi <sofian.khalfi1@gmail.com>
2020-11-12 18:12:53 +0100

    On fait pas des apis nous?

--- src/main/java/android/rest/webService/dao/utilisateur/UtilisateurRepository.java
+++ src/main/java/android/rest/webService/dao/utilisateur/UtilisateurRepository.java
@@ -2,13 +2,18 @@ package android.rest.webService.dao.utilisateur;
 
 import android.rest.webService.domain.utilisateur.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "utilisateurs", path = "utilisateurs")
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long>, UtilisateurRepositoryCustom {
 
-    List<Utilisateur> findByEmail(String emailAddress);
+    @RestResource(path = "byemail", rel="findByEmail")
+    List<Utilisateur> findByEmail(@Param("email") String emailAddress);
 
 }
